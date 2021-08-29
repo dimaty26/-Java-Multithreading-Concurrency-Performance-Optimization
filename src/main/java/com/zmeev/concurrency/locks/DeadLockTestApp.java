6d84d895252a273417b0cbf4784b2c892ef806d3
dimaty26 <dimaty26rus@mail.ru>
2021-08-29 22:11:58 +0300

    Concurrency Challenges

---
+++ src/main/java/com/zmeev/concurrency/locks/DeadLockTestApp.java
@@ -0,0 +1,95 @@
+package com.zmeev.concurrency.locks;
+
+import java.util.Random;
+
+public class DeadLockTestApp {
+
+    public static void main(String[] args) {
+        Intersection intersection = new Intersection();
+        Thread threadA = new Thread(new TrainA(intersection));
+        Thread threadB = new Thread(new TrainB(intersection));
+
+        threadA.start();
+        threadB.start();
+    }
+
+    public static class TrainA implements Runnable {
+
+        private Intersection intersection;
+        private Random random = new Random();
+
+        public TrainA(Intersection intersection) {
+            this.intersection = intersection;
+        }
+
+        @Override
+        public void run() {
+            while (true) {
+                long sleepingTime = random.nextInt(5);
+                try {
+                    Thread.sleep(sleepingTime);
+                } catch (InterruptedException e) {
+                }
+                intersection.takeRoadA();
+            }
+        }
+    }
+
+    public static class TrainB implements Runnable {
+
+        private Intersection intersection;
+        private Random random = new Random();
+
+        public TrainB(Intersection intersection) {
+            this.intersection = intersection;
+        }
+
+        @Override
+        public void run() {
+            while (true) {
+                long sleepingTime = random.nextInt(5);
+                try {
+                    Thread.sleep(sleepingTime);
+                } catch (InterruptedException e) {
+
+                }
+                intersection.takeRoadB();
+            }
+        }
+    }
+
+    public static class Intersection {
+        private Object roadA = new Object();
+        private Object roadB = new Object();
+
+        public void takeRoadA() {
+            synchronized (roadA) {
+                System.out.println("Road A is locked by thread " + Thread.currentThread().getName());
+
+                synchronized (roadB) {
+                    System.out.println("Train is passing through A");
+                    try {
+                        Thread.sleep(1);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+
+        public void takeRoadB() {
+            synchronized (roadA) {
+                System.out.println("Road A is locked by thread " + Thread.currentThread().getName());
+
+                synchronized (roadB) {
+                    System.out.println("Train is passing through B");
+                    try {
+                        Thread.sleep(1);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+}
